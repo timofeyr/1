a5859835be953f78e7dc8d51a946c16aca5daad5
timofeyr <44941447+timofeyr@users.noreply.github.com>
2020-05-18 20:48:16 +0300

    Update array_mod.py

--- array_mod.py
+++ array_mod.py
@@ -5,6 +5,6 @@ n = int(input('Input range '))      #Quantity of numbers
 myList = am.ListModifier(n)         #Assign a module to a variable
 myList.makeList()                   #Makes a list
 myList.showList()                   #Shows a list    
-myList.checkForDublicates()         #Checks, are there any dublicates numbers 
+myList.checkForDublicates()         #Checks, are there any dublicate numbers 
 myList.createDublicatesListFile()   #Makes files that contain dublicate numbers
 myList.maxListElement()             #Max element of list
